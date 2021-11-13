2ba877e35bafcaf5c6b50df3765f62368ee6e831
nagi0101 <>
2021-11-13 14:01:15 +0000

    Create Engine class
    Why: Create meta-game class to manage other classes related with game and to support useful widly-used utilities.
    
    Description: Engine class has AGame, AController, AScreen class as friend class.
    For now, it only has capability that serve current AGame instance, but other capabilities will be added.
    
    Tags: Add

---
+++ Engine/src/Engine.cpp
@@ -0,0 +1,3 @@
+#include "Engine.hpp"
+
+AGame *Engine::currentGame;
---
+++ Engine/src/Engine.hpp
@@ -0,0 +1,16 @@
+class AGame;
+class AController;
+class AScreen;
+
+class Engine 
+{
+    friend class AGame;
+    friend class AController;
+    friend class AScreen;
+public:
+    static AGame *currentGame;
+
+public:
+    static AGame *GetCurrentGame() { return Engine::currentGame; };
+    static void SetCurrentGame(AGame *game) { Engine::currentGame = game; };
+};
