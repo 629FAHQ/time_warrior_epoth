e76b3fcd3e0799b2ed203a9a83fec2e6958e39a1
nagi0101 <>
2021-11-13 13:45:51 +0000

    Struct INTPOS and INTSIZE
    Why: Create structure for saving point and length on plane.
    
    Description: Each INTPOS and INTSIZE structure save point and length int data.
    +=, -=, +, and - operator overrided so that we can add and subtract with same structure.
    
    Tags: Add

--- Engine/src/common.hpp
+++ Engine/src/common.hpp
@@ -4,8 +4,30 @@
 #define SAFE_DELETE(p) if(p) { delete (p); (p) = NULL; }
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
 
+#define MAX_HEIGHT 20
+#define MAX_WIDTH 80
 
-struct INTPOS {
-    int x;
+
+class INTPOS {
+public:
     int y;
+    int x;
+public:
+    INTPOS(int y = 0, int x = 0);
+    INTPOS operator+=(const INTPOS& right);
+    INTPOS operator-=(const INTPOS &right);
+    const INTPOS operator+(const INTPOS& right) const;
+    const INTPOS operator-(const INTPOS& right) const;
+};
+
+class INTSIZE {
+public:
+    int height;
+    int width;
+public:
+    INTSIZE(int height = 0, int width = 0);
+    INTSIZE operator+=(const INTSIZE& right);
+    INTSIZE operator-=(const INTSIZE& right);
+    const INTSIZE operator+(const INTSIZE& right) const;
+    const INTSIZE operator-(const INTSIZE& right) const;
 };
